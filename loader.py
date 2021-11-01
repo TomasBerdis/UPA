610b382cc8855451b0eccd6c93c2f57e5b7bd2e2
jirkaseta <jiri.setinsky@gmail.com>
2021-11-01 13:01:22 +0100

    Actualization of data in loader.py and added online MongoDB cluster

--- loader.py
+++ loader.py
@@ -4,7 +4,9 @@ import sys
 
 # connect to MongoDB
 print('Connecting to the database...', file=sys.stdout)
-client = MongoClient('localhost', 27017)
+#client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb+srv://demo:demo@cluster0.qapgw.mongodb.net/upa?retryWrites=true&w=majority")
+
 
 # create/get database
 db = client['upa']
@@ -20,7 +22,8 @@ collection_cols = [['datum','prirustkovy_pocet_nakazenych', 'prirustkovy_pocet_v
                  , ['datum','pacient_prvni_zaznam']
                  , ['datum','pocet_PCR_testy','pocet_AG_testy']]
 
-for id, name in enumerate(collection_names):
+def drop_and_create(id, name):
+    
     # drop old collections
     db.drop_collection(name)
 
@@ -33,4 +36,21 @@ for id, name in enumerate(collection_names):
     data_dict = data_from_csv.to_dict('records')
     db[name].insert_many(data_dict)
 
-print('Data successfully added to the database.', file=sys.stdout)
+    print('Data', name, 'was successfully updated to the database.', file=sys.stdout)
+
+def update_data():
+    print('Data update', file=sys.stdout)
+
+    for id,collection_name in enumerate(collection_names):
+        col=db[collection_name]
+        doc_cnt=col.count_documents({})
+        data_from_csv = pandas.read_csv(collection_sources[id], usecols=collection_cols[id])
+        online_data_amount=len(data_from_csv)
+        if online_data_amount == doc_cnt:
+            print("Collection", col.name, 'is updated  with',doc_cnt, "documents.")
+        else:
+            number_of_new_docs=online_data_amount-doc_cnt
+            print("Collection", col.name, "differs by",number_of_new_docs, "documnets.")
+            drop_and_create(id,collection_name)
+            
+update_data()
